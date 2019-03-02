fe6603de0e66173f1f54b0ae9c08e3a31c22a8d5
贾** <43312110+itxiaojia@users.noreply.github.com>
2019-03-02 10:15:50 +0800

    你好
    嗯嗯嗯

--- springboot/ srv/ java/Hello.java
+++ springboot/ srv/ java/Hello.java
@@ -2,9 +2,9 @@
  * @Author: Mr.jia
  * @Date: 2019/3/2 10:11
  */
-public class Hello {
-
-	public static void main(String[] args) {
+公共 类 Hello {
+你好
+	public  static  void  main（String [] args）{
 		System.out.println("你好");
 	}
 }
